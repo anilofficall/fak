f6a9474a837c76d26169866b77a01ae09d8c6b52
Glitch (celestial-night) <none>
2019-12-06 18:47:06 +0000

    🏑🛤 Checkpoint ./komutlar/çayiç.js:8038619/1083 ./komutlar/davet.js:8038619/1075 ./komutlar/eğlence.js:8038619/1235 ./komutlar/kullanıcıbilgim.js:8038619/1607 ./komutlar/reboot.js:8038619/932 ./komutlar/reklamtaramsı.js:8038619/659 ./komutlar/saat.js:8038619/475 ./komutlar/sunucubilgi.js:8038619/1430 ./komutlar/üyedurum.js:8038619/1176 ./komutlar/yardım.js:8038619/751

---
+++ komutlar/yardım.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+
+
+exports.run = function(client, message) {
+const embed = new Discord.RichEmbed()
+.setColor('RANDOM')
+.setTitle('» Komut Grupları')
+.setTimestamp()
+.addField('» Eğlence Komutları', 'r!eğlence')
+.addField('» Moderatör Komutları', 'r!moderatör')
+.addField('» Genel Komutlar', 'r!genel')
+.addField('» Ekstra Komutlar', 'r!ekstra')
+.addField('» Nsfw Komutları', 'r!nsfw')
+.setFooter('© 2018 Verified', client.user.avatarURL)
+.setTimestamp()
+.setThumbnail(client.user.avatarURL)
+message.channel.send(embed)
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'yardım',
+  description: 'Tüm komutları gösterir.',
+  usage: 'yardım'
+};
