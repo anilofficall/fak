c104efdf4afa2a3f946eb8b9da173fbd37af37d2
Glitch (celestial-night) <none>
2019-12-17 15:09:47 +0000

    🏥🏹 Checkpoint ./komutlar/pixel.js:8038619/370 ./komutlar/rip.js:8038619/13278 ./komutlar/slowmode.js:8038619/2580 ./komutlar/wasted.js:8038619/1172 ./komutlar/şanslısayım.js:8038619/15418 ./komutlar/eğlence.js:8038619/437

--- komutlar/eğlence.js
+++ komutlar/eğlence.js
@@ -10,6 +10,10 @@ exports.run = (client, message, args) => {
     .addField('**r!wtcN**', '```Rastgele wtcN fotoğrafları atar.```')//ne kadar eğlence komutunuz varsa o kadar .addField('prefix+komut', 'açıklama/kullanım amacı') koyun
     .addField('**r!slots**', '```Slot Machine.```')
     .addField('**r!kaçcm**', '```Malatafının Kaç Cm olduğunu Söyler.```')
+     .addField('**r!espiri**', '```Bot Espiri Yapar.```')
+    .addField('**r!fbi**', '```Fbi Open The door.```') 
+      .addField('**r!kafanasınasık**', '```İstediğiniz Kişinin Kafasına Sıkar.```')
+      .addField('**r!kavgaet**', '```Kavga Edersiniz.```')
     .addField(`» Linkler`, `[Bot Davet Linki](https://discordapp.com/oauth2/authorize?client_id=652511470579613716${botid}&scope=bot&permissions=8) **|** [Destek Sunucusu](https://discord.gg/BAĞLANTI) **|** [Bota Oy Ver (Vote)](https://discordbots.org/bot/${botid}/vote) **|** [Web Sitesi]()`)//websiteniz yoksa  **|** [Web Sitesi]() yeri silebilirsiniz
     message.channel.sendEmbed(embed);
 
