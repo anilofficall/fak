9020bfafd308da52cf7405e0d9ce89e160d87b0b
Glitch (celestial-night) <none>
2019-12-21 20:22:45 +0000

    🐸🏉 Checkpoint ./komutlar/genel.js:8038619/159 ./komutlar/korkut.js:8038619/10

--- komutlar/genel.js
+++ komutlar/genel.js
@@ -10,6 +10,7 @@ exports.run = (client, message, args) => {
     .addField('**r!havadurumu**', '```Hava Durumunu Gösterir.```')//ne kadar eğlence komutunuz varsa o kadar .addField('prefix+komut', 'açıklama/kullanım amacı') koyun
     .addField('**r!avatar**', '```Profil Resminizi Atar..```')
     .addField('**r!kaçcm**', '```Malatafının Kaç Cm olduğunu Söyler.```')
+     .addField('**r!istatistik**', '```Bot Üzerindeki İstatistik.```')
     .addField(`» Linkler`, `[Bot Davet Linki](https://discordapp.com/oauth2/authorize?client_id=652511470579613716${botid}&scope=bot&permissions=8) **|** [Destek Sunucusu](https://discord.gg/BAĞLANTI) **|** [Bota Oy Ver (Vote)](https://discordbots.org/bot/${botid}/vote) **|** [Web Sitesi]()`)//websiteniz yoksa  **|** [Web Sitesi]() yeri silebilirsiniz
     message.channel.sendEmbed(embed);
 
