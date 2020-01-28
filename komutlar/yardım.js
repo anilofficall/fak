06d517894c7a775605c2a523c87a26ce324529a2
Glitch (celestial-night) <none>
2020-01-28 12:23:27 +0000

    🌨🍴 Checkpoint ./komutlar/yardım.js:4311127/46

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -3,12 +3,12 @@ const db = require('quick.db');
 
 exports.run = async(client, message, args) => {
     const Embed = new Discord.RichEmbed()
-    .setAuthor("Stat Bot", client.user.avatarURL)
+    .setAuthor("DarkCodeS-Panel", client.user.avatarURL)
     .setTitle("DarkCodeS-Panel")
     .setColor("BLUE")
     .setDescription("Sunucu İstatistiklerini Sunan Bir Botdur.")
     .addField("dcp!kurulum / dcp!statskapat", "Stats Odası Açıp Kapatmak İçin Bu 2 Komut Yeterlidir.")
-    .addField("dcp!botbilgi", "Stat Bot Hakkında Bilgi Alırsınız!")
+    .addField("dcp!botbilgi", "DarkCodeS-Panel Hakkında Bilgi Alırsınız!")
     .setTimestamp()
     .setFooter("© DarkCodeS-Panel", client.user.avatarURL)
     message.channel.send(Embed)
